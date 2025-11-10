cdd78a1f7c41ccafe20f2c8f1eae4b994de7bec6
Багаутдинов-Ринат <tensal@bk.ru>
2025-11-10 22:05:19 +0300

    что бы фотки показывали в новостях

--- transagency/urls.py
+++ transagency/urls.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ДЛЯ PRODUCTION - обслуживаем медиа файлы
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
