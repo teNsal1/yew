8ba481fa4691e4d7f185c39f9f2931dab0f32db0
Багаутдинов-Ринат <tensal@bk.ru>
2025-11-10 21:45:51 +0300

    3

--- transagency/settings.py
+++ transagency/settings.py
@@ -42,17 +42,17 @@ DATABASES = {
 }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ewnu&($lr$tn6%&w=ewg^-n^fiw**4$-(8cu#gpmvyi3&z5gu'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 
 ALLOWED_HOSTS = [
@@ -62,6 +62,19 @@ ALLOWED_HOSTS = [
     "xn--80aagdk9bdjkmedib.com",
     "www.xn--80aagdk9bdjkmedib.com",
 ]
+
+# После ALLOWED_HOSTS добавь:
+CSRF_TRUSTED_ORIGINS = [
+    "https://yew.onrender.com",
+    "https://трансагентство.com",
+    "https://www.трансагентство.com", 
+    "https://xn--80aagdk9bdjkmedib.com",
+    "https://www.xn--80aagdk9bdjkmedib.com",
+]
+
+# Для статики на production
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Application definition
@@ -78,6 +91,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -110,12 +124,6 @@ WSGI_APPLICATION = 'transagency.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Автоматически создаст файл базы данных в корне проекта
-    }
-}
 
 
 # Password validation
